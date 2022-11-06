ec48d653ce51665b09ee776e71bd64168c591f0d
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2022-11-06 22:29:43 +0530

    Updates

---
+++ _app/immutable/chunks/1-b97b426b.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-85be2125.js";export{t as component};
--- _app/immutable/chunks/4-82c54105.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/candidate/_page.svelte-6fe5c63c.js";export{t as component};
