ea5a23d0c59eef96a2dea8b84c02d9ed9498b949
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2023-02-19 07:22:34 +0000

    Deploying to gh-pages from  @ e16dfb2384920cd8a61100a395fb7a9f89ba706e 🚀

---
+++ _app/immutable/chunks/10-6433b0c0.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/volunteer/_page.svelte-0ba89fb4.js";export{t as component};
---
+++ _app/immutable/chunks/3-d3b665f7.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/candidate/_page.svelte-eb2f7ac6.js";export{t as component};
